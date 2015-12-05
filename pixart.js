6c0e2fd97965746593fff75f2eaa28b4341580fa
Klajdi Bejko <kbejko@gmail.com>
2015-12-05 16:18:23 -0500

    Commit 3

--- pixart.js
+++ pixart.js
@@ -2,3 +2,10 @@ $("#form").submit(function(event){
   event.preventDefault();
   $(".brush").css("background", $("#color-field").val());
 })
+
+
+// var squareDiv = "<div class=square></div>";
+
+for (var i = 0; i < 21; i++) {
+  $("body").append("<div class=square></div>");
+}
