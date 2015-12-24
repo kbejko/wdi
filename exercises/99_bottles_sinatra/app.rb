e58f195b39942b2efb02e9425af8b765ed61d11b
Klajdi Bejko <kbejko@gmail.com>
2015-12-24 15:01:33 -0500

    Finish pair programming bot. Add views to 99 bottles exercise

--- exercises/99_bottles_sinatra/app.rb
+++ exercises/99_bottles_sinatra/app.rb
@@ -2,14 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  "<h1>99 Bottle of Beer on the Wall</h1> <a href='/98'>Take one Down</a>"
+  erb :index
 end
 
 get '/:num_bottles' do
-  num_bottles = params[:num_bottles].to_i
-  if num_bottles == 0
-    "<h1>No Bottles on the Wall</h1> <a href='/99'>Start Over</a>"
-  else
-    "<h1>#{num_bottles} Bottles of Beer on the Wall</h1> <a href='/#{num_bottles-1}'>Take one Down</a>"
-  end
+  @num_bottles = params[:num_bottles].to_i
+  @next = @num_bottles - 1
+  erb :index
 end
