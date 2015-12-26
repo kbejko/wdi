0daf2d5177923f48e42a6c06dd976713b45657c2
Klajdi Bejko <kbejko@gmail.com>
2015-12-26 14:52:58 -0500

    Add home page view, add route to app.rb file, update layout

--- exercises/tunr/app.rb
+++ exercises/tunr/app.rb
@@ -1,3 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
+
+require_relative 'db/connection.rb'
+
+require_relative 'models/artist'
+require_relative 'models/song'
+
+get '/' do
+  erb :home
+end
