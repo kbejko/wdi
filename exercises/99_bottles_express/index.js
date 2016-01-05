6283ffa1ec725734bd11d0467d3321e9f236ab1c
kbejko <kbejko@gmail.com>
2016-01-04 20:07:11 -0500

    Add player form

--- exercises/99_bottles_express/index.js
+++ exercises/99_bottles_express/index.js
@@ -2,13 +2,24 @@ var express = require("express")
 var app = express()
 var hbs = require("hbs")
 var bottles = require("./controllers/bottles.js")
+var bodyParser = require("body-parser")
 
 app.set("view engine", "hbs")
 app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(4000)
 
 app.get("/", function(req, res){
-  res.redirect("/99")
+  res.render("welcome")
+})
+
+app.post("/", function(req, res){
+  res.render("index", {
+    player_name: req.body.player_name,
+    bottles: 99,
+    next: 98
+  })
 })
 
 // app.get("/:number_of_bottles?", function(req, res){
