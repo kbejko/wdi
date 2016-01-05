bb586c2a94e827d39b266a3c89e72e1c10881a5d
kbejko <kbejko@gmail.com>
2016-01-04 19:18:59 -0500

    Add handlebars solution

--- exercises/99_bottles_express/index.js
+++ exercises/99_bottles_express/index.js
@@ -1,18 +1,26 @@
 var express = require("express")
 var app = express()
-
+var hbs = require("hbs")
+app.set("view engine", "hbs")
+app.use(express.static(__dirname + "/public"))
 app.listen(4000)
 
 app.get("/", function(req, res){
   res.redirect("/99")
 })
 
-app.get("/:number_of_bottles?", function(req, res){
-  var numBottles = req.params.number_of_bottles || 99
-  var takeOneDown = numBottles - 1
-  if (numBottles > 1) {
-    res.send(numBottles + " Bottles of beer on the wall <a href='/" + takeOneDown + "'> Take one down pass it around")
-  } else {
-    res.send("1 bottle of beer on the wall <a href='/'>Start Over")
-  }
+// app.get("/:number_of_bottles?", function(req, res){
+//   var numBottles = req.params.number_of_bottles || 99
+//   var takeOneDown = numBottles - 1
+//   if (numBottles > 1) {
+//     res.send(numBottles + " Bottles of beer on the wall <a href='/" + takeOneDown + "'> Take one down pass it around")
+//   } else {
+//     res.send("1 bottle of beer on the wall <a href='/'>Start Over")
+//   }
+// })
+
+app.get("/:numberOfBottles?", function(req, res){
+  var numberOfBottles = req.params.numberOfBottles || 99
+  var next = numberOfBottles - 1
+  res.render("index", {bottles: numberOfBottles, next: next})
 })
