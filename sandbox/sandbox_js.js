b838c047fb0f5419ec8e9416eae3e0d2a907f694
Klajdi Bejko <kbejko@gmail.com>
2016-01-07 10:24:01 -0500

    Add comments

--- sandbox/sandbox_js.js
+++ sandbox/sandbox_js.js
@@ -1,13 +1,17 @@
 function squeeze(string){
   var doubleLetters = []
   var newString = ""
+  // iterates over string array
   for (var i = 0; i < string.length; i++) {
+    // if letter is not equal to neighbor on the right, add that letter to the newString variable
     if (string[i] !== string[i+1]) {
       newString += string[i]
+      // if it does equal to neighbor, add letter to doubleLetters array
     } else {
       doubleLetters += string[i]
     }
   }
+  // return array or newString and the letters removed
   return [newString, doubleLetters]
 }
 
